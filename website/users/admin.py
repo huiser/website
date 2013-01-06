de57812faec564ccf20e473f5d8b9b07b84e4f71
Stephan Huiser <stephan@huiser.nl>
2013-01-06 16:54:39 +0100

    Changed User model + admin

--- website/users/admin.py
+++ website/users/admin.py
@@ -2,11 +2,26 @@ from django.contrib import admin
 from users.models import CustomUser
 
 class CustomUserAdmin(admin.ModelAdmin):
-    fields = ['last_name', 'first_name', 'is_active', 'is_staff', 'date_joined', 'date_left', 'mobile', 'mobile_public', 'ssh_public_key', ]
-    list_display = ['username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff',]
-    list_display_links = ['username',]
-    ordering = ['last_name', 'first_name' ]
-    search = ['username', 'email', 'last_name', 'first_name', 'mobile', ]
+    fieldsets = (
+        (None, {
+            'fields': ('username', 'first_name', 'last_name', 'email', 'is_active', 'is_staff', 'date_joined', 'date_left', ),
+        }),
+        ('Communication', {
+            'classes': ('collapse',),
+            'fields': (('mobile_phone', 'publish_mobile_phone', ), ('home_phone', 'publish_home_phone')),
+        }),
+        ('Security', {
+            'classes': ('collapse',),
+            'fields': ('ssh_public_key', ),
+        }),
+    )
+    #fields = ['last_name', 'first_name', 'is_active', 'is_staff', 'date_joined', 'date_left', 'mobile', 'mobile_public', 'ssh_public_key', ]
+    readonly_fields = ('date_joined',)
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff',)
+    list_display_links = ('username',)
+    list_filter = ('is_staff', 'is_active', )
+    ordering = ('last_name', 'first_name' )
+    search = ('username', 'email', 'last_name', 'first_name', 'mobile', )
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- website/users/models.py
+++ website/users/models.py
@@ -5,8 +5,10 @@ import datetime
 from django.utils.timezone import utc
 
 class CustomUser(AbstractUser):
-    mobile = models.CharField(_('mobile phone'), max_length=20, blank=True)
-    mobile_public = models.BooleanField(_('public mobile phone'), default=False)
+    mobile_phone = models.CharField(_('mobile phone'), max_length=20, blank=True)
+    publish_mobile_phone = models.BooleanField(_('publish mobile phone'), default=False)
+    home_phone = models.CharField(_('home phone'), max_length=20, blank=True)
+    publish_home_phone = models.BooleanField(_('publish mobile phone'), default=False)
     ssh_public_key = models.TextField(_('public ssh key'), blank=True)
     date_left = models.DateTimeField(_('date left'),
         default = datetime.datetime.utcnow().replace(tzinfo=utc) + datetime.timedelta(days=10000))
